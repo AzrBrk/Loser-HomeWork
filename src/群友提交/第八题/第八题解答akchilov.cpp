a0b6795c98ba9c5686d8f1bc8d288ac25bcfcc54
AzrBrk <pbrink51@gmail.com>
2023-08-13 20:39:24 +0800

    Create 第八题解答akchilov.cpp
    akchilov

---
+++ src/群友提交/第八题/第八题解答akchilov.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+
+template<class Ty, size_t size>
+struct array {
+    Ty* begin() { return arr; };
+    Ty* end() { return arr + size; };
+    Ty arr[size];
+};
+
+
+template<class ...L>
+array(L... l) -> array<int, sizeof ...(L)>;
+
+
+
+int main() {
+    ::array arr{ 1, 2, 3, 4, 5 };
+    for (const auto& i : arr) {
+        std::cout << i << ' ';
+    }
+    return 0;
+}
